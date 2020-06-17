1e9135cb91bcba14a65160824db33ba3b78211ba
hyeonsook95 <qu3230@gmail.com>
2020-06-17 22:55:05 +0900

    Updates

---
+++ precache-manifest.3c923a113674516c1e82340dea1f9238.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bb038f87dcce3aa99506242de695c6f",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "e3eb9cab60242b7483c1",
+    "url": "/movie_app/static/css/main.a5a5e5b1.chunk.css"
+  },
+  {
+    "revision": "b465fb3a41db387c5800",
+    "url": "/movie_app/static/js/2.6760420d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.6760420d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3eb9cab60242b7483c1",
+    "url": "/movie_app/static/js/main.4d9a99fd.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
